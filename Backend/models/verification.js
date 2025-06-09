ee4e0e60bba50d00276ed1861898d68fd26ed3c2
SREEKANTH PS <sreekanth3265@gmail.com>
2025-06-09 23:54:31 +0530

    add userModel and verification model

---
+++ Backend/models/verification.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const verificationSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Verification = mongoose.model("Verification", verificationSchema);
+export default Verification;
