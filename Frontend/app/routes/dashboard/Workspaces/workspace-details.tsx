ff0a61ba935042c323ab5def43c05cf7f37573b1
SREEKANTH PS <sreekanth3265@gmail.com>
2025-06-11 13:47:13 +0530

    add workspace detail page and header

---
+++ Frontend/app/routes/dashboard/Workspaces/workspace-details.tsx
@@ -0,0 +1,40 @@
+import { Loader } from "@/components/customReusable/loader";
+import { WorkspaceHeader } from "@/components/Workspace/workspace-header";
+import { useGetWorkspaceByIdQuery } from "@/hooks/use-workspace";
+import type { Project, Workspace } from "@/types";
+import { useState } from "react";
+import { useParams } from "react-router";
+
+const WorkspaceDetails = () => {
+  const { workspaceId } = useParams<{ workspaceId: string }>();
+  const [isCreateProject, setIsCreateProject] = useState(false);
+  const [isInviteMember, setIsInviteMember] = useState(false);
+  if (!workspaceId) {
+    return <div>No Workspace found</div>;
+  }
+  const { data, isLoading } = useGetWorkspaceByIdQuery(workspaceId) as {
+    data: {
+      workspace: Workspace;
+      projects: Project[];
+    };
+    isLoading: boolean;
+  };
+  if (isLoading) {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  }
+  return (
+    <div className="space-y-8">
+      <WorkspaceHeader
+        workspace={data?.workspace}
+        members={data?.workspace?.members as any}
+        onCreateProject={() => setIsCreateProject(true)}
+        onInviteMember={() => setIsInviteMember(true)}
+      />
+    </div>
+  );
+};
+export default WorkspaceDetails;
