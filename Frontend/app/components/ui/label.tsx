98cd7d5ddcaac0fa682f563bec0345651b2f60ed
SREEKANTH PS <sreekanth3265@gmail.com>
2025-08-13 00:42:25 +0530

    edited vite config and add sourceMap

--- Frontend/app/components/ui/label.tsx
+++ Frontend/app/components/ui/label.tsx
@@ -1,14 +1,12 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/lib/utils"
+type LabelProps = React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>;
 
-function Label({
-  className,
-  ...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+function Label({ className, ...props }: LabelProps) {
   return (
     <LabelPrimitive.Root
       data-slot="label"
@@ -18,7 +16,7 @@ function Label({
       )}
       {...props}
     />
-  )
+  );
 }
 
-export { Label }
+export { Label };
